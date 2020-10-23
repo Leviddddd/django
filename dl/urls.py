70ff9d97bb7d44a236a714d1ce67cff9ea7fc0dd
Levid <565132033@qq.com>
2020-10-23 16:33:14 +0800

    添加定时器demo

--- dl/urls.py
+++ dl/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path
 from . import views
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
+
+
+def tick():
+    print('Tick! The time is: %s' % datetime.now())
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(tick, 'interval', seconds=30)
+scheduler.start()
 
 urlpatterns = [
     path('', views.login, name='login'),
